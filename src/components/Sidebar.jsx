66e6be2014f82f50201a98af2b7964ecc87b3977
firdoush <mdfirdoushali1776@gmail.com>
2024-11-11 23:34:43 +0530

    added backend url

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("http://localhost:4000/api/v1/user/admin/logout", {
+      .get("https://hospital-management-system-backend-qc8f.onrender.com/api/v1/user/admin/logout", {
         withCredentials: true,
       })
       .then((res) => {
